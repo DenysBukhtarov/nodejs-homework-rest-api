cc6e29f7c16fd6cd8b822d5df13d96f43f00f1ec
DENYS BUKHTAROV <denis180984@gmail.com>
2023-04-28 12:32:55 +0300

    all fix

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,16 @@
+const validateAddContact = require("./validateAddContact");
+const validateUpdateContact = require("./validateUpdateContact");
+const validateFieldsContact = require("./validateFieldsContact");
+const validateUpdateFavorite = require("./validateUpdateFavorite");
+const validateFieldsFavorite = require("./validateFieldsFavorite,");
+const isValidId = require("./isValidId");
+
 
 module.exports = {
-    validateAddContact,
-    validateUpdateContact,
-validateFieldsContact,
-}
+  validateAddContact,
+  validateUpdateContact,
+  validateFieldsContact,
+  validateUpdateFavorite,
+  validateFieldsFavorite,
+  isValidId,
+};
