90136f27e997b42cf8b355c02670ca369d16cf4a
DENYS BUKHTAROV <denis180984@gmail.com>
2023-05-03 09:29:10 +0300

    All

---
+++ middlewares/index.js
@@ -0,0 +1,16 @@
+const validateAddContact = require("./validateAddContact ");
+const validateUpdateContact = require("./validateUpdateContact");
+const validateFieldsContact = require("./validateFieldsContact");
+const validateUpdateFavorite = require("./validateUpdateFavorite");
+const validateFieldsFavorite = require("./validateFieldsFavorite");
+const isValidId = require("./isValidId");
+
+
+module.exports = {
+  validateAddContact,
+  validateUpdateContact,
+  validateFieldsContact,
+  validateUpdateFavorite,
+  validateFieldsFavorite,
+  isValidId,
+};
